9d3a6922a6be362afdefe2cd96512d057126807c
Charles Stolz <charles.stolz@protonmail.com>
2022-11-07 21:26:48 -0700

    adding test script for extracting sample data

---
+++ extract_data/extract_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import praw
+from praw.models import MoreComments
+import json
+import pandas as pd
+
+
+def extract_headlines():
+    headlines = set()
+
+    for submission in reddit.subreddit("UkrainianConflict").new(limit=100):
+        headlines.add(submission.title)
+
+    df = pd.DataFrame(data=list(headlines))
+    df.to_csv("./data/UkrainianConflict-headlines.csv", index=False)
+
+
+def extract_comments():
+    comments = []
+
+    for top_level_comment in reddit.subreddit("UkrainianConflict").stream.comments():
+        if len(comments) == 100:
+            break
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        comments.append(top_level_comment.body)
+
+    df = pd.DataFrame(data=comments)
+    df.to_csv("./data/UkrainianConflict-comments.csv", index=False)
+
+
+if __name__ == "__main__":
+    with open("creds.json") as fh:
+        creds = json.loads(fh.read())[0]
+
+    reddit = praw.Reddit(
+        client_id=creds["client_id"],
+        client_secret=creds["client_secret"],
+        user_agent=creds["user_agent"],
+    )
+    extract_headlines()
+    extract_comments()
