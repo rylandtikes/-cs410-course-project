1fdc2414f762cf541e1ab36c370d662b4788aade
Charles Stolz <charles.stolz@protonmail.com>
2022-11-08 20:51:26 -0700

    adding new datasets

--- extract_data/extract_test.py
+++ extract_data/extract_test.py
@@ -5,11 +5,12 @@ from praw.models import MoreComments
 import json
 import pandas as pd
 
+DATASET_SIZE = 1000
 
 def extract_headlines():
     headlines = set()
 
-    for submission in reddit.subreddit("UkrainianConflict").new(limit=100):
+    for submission in reddit.subreddit("UkrainianConflict").new(limit=DATASET_SIZE):
         headlines.add(submission.title)
 
     df = pd.DataFrame(data=list(headlines))
@@ -20,11 +21,12 @@ def extract_comments():
     comments = []
 
     for top_level_comment in reddit.subreddit("UkrainianConflict").stream.comments():
-        if len(comments) == 100:
+        if len(comments) == DATASET_SIZE:
             break
         if isinstance(top_level_comment, MoreComments):
             continue
         comments.append(top_level_comment.body)
+        print(len(comments))
 
     df = pd.DataFrame(data=comments)
     df.to_csv("./data/UkrainianConflict-comments.csv", index=False)
